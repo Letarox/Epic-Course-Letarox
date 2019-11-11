1ba78110cb5cfa430de26268075921ec6be4b05f
Renato Figueiredo dos Santos <letarox@gmail.com>
2019-11-11 17:20:23 -0300

    Mouse Movement Added

---
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/Epic_Tower_Defense/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 5f;
+    private float _limitLeft = -38f, _limitRight = -34f, _limitUp = 27f, _limitDown = 12f;
+    private float _moveX, _moveY;
+    private Vector3 _cameraPos;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        //get the input from WASD keys
+        _moveX = Input.GetAxis("Horizontal");
+        _moveY = Input.GetAxis("Vertical");
+
+        //move the camera based on the keys
+        transform.Translate(new Vector3(_moveX, _moveY, 0));
+    }
+}
--- Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/Epic_Tower_Defense/Scripts/MouseControl.cs
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/Epic_Tower_Defense/Scripts/MouseControl.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class MouseControl : MonoBehaviour
 {
     [SerializeField]
-    private float _speed = 2.5f;
+    private float _speed = 2.5f, _maxSpeed = 5f;
     private float _width, _height, _validLeft, _maxLeft, _validRight, _maxRight, _validTop, _maxTop, _validBottom, _maxBottom;
-    private Vector3 _mousePos;
+    private float _limitX, _limitY;
+    private Vector3 _mousePos, _cameraPos;
+    private float _maxZoom = 15f, _minZoom = 30f;
     void Start()
     {
         _width = Screen.width;
@@ -17,27 +19,38 @@ public class MouseControl : MonoBehaviour
         _validTop = _height * 0.9f; // values between 0 and 10% of the height
         _validBottom = _height * 0.1f; // values between 0 and 10% of the height
 
-        _maxLeft = _width * 0.03f; // values to move faster than the start movement
-        _maxRight = _width * 0.03f; // values to move faster than the start movement
-        _maxTop = _height * 0.97f; // values to move faster than the start movement
-        _maxBottom = _height * 0.03f; // values to move faster than the start movement
+        _maxLeft = _width * 0.03f; // values between 0 and 3% of the width for faster movement at the edge of the screen
+        _maxRight = _width * 0.97f; // values between 0 and 3% of the width for faster movement at the edge of the screen
+        _maxTop = _height * 0.97f; // values between 0 and 3% of the height for faster movement at the edge of the screen
+        _maxBottom = _height * 0.03f; // values between 0 and 3% of the height for faster movement at the edge of the screen
     }
 
     // Update is called once per frame
     void Update()
     {
         MouseMovement();
+        MouseZoom();
+    }
+
+    private void MouseZoom()
+    {
+        _cameraPos = transform.position;
+        _cameraPos.y -= Input.mouseScrollDelta.y;
+        if (_cameraPos.y < _maxZoom)
+            _cameraPos.y = _maxZoom;
+        if (_cameraPos.y > _minZoom)
+            _cameraPos.y = _minZoom;
+        transform.position = _cameraPos;
     }
 
     private void MouseMovement()
     {
-
         _mousePos = Input.mousePosition;
         if(_mousePos.x < _validLeft)
         {
             if(_mousePos.x < _maxLeft)
             {
-                transform.Translate(Vector3.left * (2 * _speed) * Time.deltaTime);
+                transform.Translate(Vector3.left * _maxSpeed * Time.deltaTime);
             }
             else
             {
@@ -48,7 +61,7 @@ public class MouseControl : MonoBehaviour
         {
             if (_mousePos.x > _maxRight)
             {
-                transform.Translate(Vector3.right * (2 * _speed) * Time.deltaTime);
+                transform.Translate(Vector3.right * _maxSpeed * Time.deltaTime);
             }
             else
             {
@@ -60,7 +73,7 @@ public class MouseControl : MonoBehaviour
         {
             if (_mousePos.y > _maxTop)
             {
-                transform.Translate(Vector3.up * (2 * _speed) * Time.deltaTime);
+                transform.Translate(Vector3.up * _maxSpeed * Time.deltaTime);
             }
             else
             {
@@ -71,7 +84,7 @@ public class MouseControl : MonoBehaviour
         {
             if (_mousePos.y < _maxBottom)
             {
-                transform.Translate(Vector3.down * (2 * _speed) * Time.deltaTime);
+                transform.Translate(Vector3.down * _maxSpeed * Time.deltaTime);
             }
             else
             {
